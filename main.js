27c44a6d7a7a09da05fc8f9c91751113b1cadb04
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-04-20 12:49:04 -0400

    sigh

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
+makeRunOn = (str) => {
+  return str.split('.').join(',');
+}// y cant i .replace()? 
 
+stringOut = (str) => {
+  return str.split('').join(' ');
+}
 
+nightOwls = () => {
+  
+}
 
+totalScore = (totalScore) => {
+  const levels1 = [
+    {
+      score: 50,
+      multiplier: 1,
+    },
+    {
+      score: 200,
+      multiplier: 1,
+    },
+    {
+      score: 400,
+      multiplier: 1,
+    },
+    {
+      score: 350,
+      multiplier: 1,
+    },
+  ]
+  if(levels1.multiplier === 1){
+    return levels1.score === 1000
 
+  }
 
+}
 
+const Faqtory = (questions) => {
+  return {
+    questions: [],
+
+    addQuestion(){
+      const question1 = 'What is your site all about?';
+      question1.push()
+    },
+
+    answerQuestion(){
+
+    },
+  }
+}
 
 if (typeof makeRunOn === 'undefined') {
   makeRunOn = undefined;
